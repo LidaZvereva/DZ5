15ec90b9919cb8ca59fec2bb278dbf0d1c3102bf
Lidia <lidia.wer@mail.ru>
2022-09-06 00:31:57 +0300

    задача 37

--- Program.cs
+++ Program.cs
@@ -63,3 +63,43 @@
 //     System.Console.WriteLine($"Сумма нечетных позиций: {count}");
 // } 
 //  SumOfNotEvenPositions(EnterRandomArray());
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+
+int [] EnterRandomArray()
+{  
+    Console.WriteLine("Введите размер массива");
+    int arraySize=Convert.ToInt32(Console.ReadLine());
+    int []array= new int [arraySize];
+    
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i]=new Random().Next(-100,100);
+       
+    }
+    System.Console.WriteLine(String.Join(",",array));
+    return array;
+
+}
+
+void PeriodOfPairs(int [] array)
+{
+    int Pair=0;
+    
+    int array2Size=(array.Length)/2;
+    int []array2=new int [array2Size];
+    int j=0;
+    
+    for (int y=0;y<array2.Length;y++)
+    {
+            Pair=array[j]*array[array.Length-1-j];
+            array2[y]=Pair;
+            j++;
+    }
+    Console.WriteLine("Новый массив ");
+    Console.WriteLine(String.Join(",",array2));
+    
+}
+ PeriodOfPairs(EnterRandomArray());
+
